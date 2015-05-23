c63b673d280fc491f1b39f5a859e6ebcca9cea8b
Ben Clark <benjamin.k.clark@gmail.com>
2015-05-23 19:08:40 -0400

    google map on /about

--- public/js/page.js
+++ public/js/page.js
@@ -99,3 +99,26 @@ $(document).ready(function() {
   replaceCeleb();
   var derp = setInterval(replaceCeleb, 5000);
 });
+
+// google map
+google.maps.event.addDomListener(window, 'load', init);
+function init() {
+  var center = new google.maps.LatLng(40.709181, -73.969326);
+  var mapOptions = {
+    center: center, // New York
+    disableDoubleClickZoom: true,
+    draggable: false,
+    mapTypeControl: false,
+    panControl: false,
+    scrollwheel: false,
+    streetViewControl: false,
+    zoom: 14,
+    zoomControl: false,
+    styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"simplified"},{"hue":"#0066ff"},{"saturation":74},{"lightness":100}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"off"},{"weight":0.6},{"saturation":-85},{"lightness":61}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"simplified"},{"color":"#5f94ff"},{"lightness":26},{"gamma":5.86}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.station.rail","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"transit.line","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"visibility":"on"}]}]
+  };
+  var mapElement = document.getElementById('map');
+  var map = new google.maps.Map(mapElement, mapOptions);
+  google.maps.event.addDomListener(window, 'resize', function() {
+    map.setCenter(center);
+  });
+}
